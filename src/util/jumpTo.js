8afc9d37c3ef02941cf2aeed865bf08511216e75
Your Name <since@qq.com>
2020-10-27 09:08:36 +0800

    页面跳转

--- src/util/jumpTo.js
+++ src/util/jumpTo.js
@@ -1,41 +1,41 @@
-const host = 'http://localhost:8081/page/';
-export const indexUrl = host + 'index/index.html';
+const host = '/';
+export const indexUrl = host + 'to/index';
 export const toindex = () => {
       window.location.href = indexUrl;
   }
-export const topicUrl = host + 'topic/index.html';
+export const topicUrl = host + 'to/topic';
 export const totopic = () => {
       window.location.href = topicUrl;
   }
-export const mineUrl = host + 'mine/index.html';
+export const mineUrl = host + 'to/mine';
 export const tomine = () => {
       window.location.href = mineUrl;
   }
-export const communityUrl = host + 'community/index.html';
+export const communityUrl = host + 'to/community';
 export const tocommunity = () => {
       window.location.href = communityUrl;
   }
-export const topicsUrl = host + 'topics/index.html';
+export const topicsUrl = host + 'to/topics';
 export const totopics = () => {
       window.location.href = topicsUrl;
   }
-export const forumUrl = host + 'forum/index.html';
+export const forumUrl = host + 'to/forum';
 export const toforum = () => {
       window.location.href = forumUrl;
   }
-export const forum_detailUrl = host + 'forum_detail/index.html';
+export const forum_detailUrl = host + 'to/forum_detail';
 export const toforum_detail = () => {
       window.location.href = forum_detailUrl;
   }
-export const searchUrl = host + 'search/index.html';
+export const searchUrl = host + 'to/search';
 export const tosearch = () => {
       window.location.href = searchUrl;
   }
-export const search_resultUrl = host + 'search_result/index.html';
+export const search_resultUrl = host + 'to/search_result';
 export const tosearch_result = () => {
       window.location.href = search_resultUrl;
   }
-export const issueQUrl = host + 'issueQ/index.html';
+export const issueQUrl = host + 'to/issueQ';
 export const toissueQ = () => {
       window.location.href = issueQUrl;
   }
