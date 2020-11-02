cf46ca71a321d37ef0b16710ca3459620e48a968
Your Name <since@qq.com>
2020-11-02 21:58:28 +0800

    跳转路径修改

--- src/util/jumpTo.js
+++ src/util/jumpTo.js
@@ -1,61 +1,61 @@
 const host = '/';
-export const indexUrl = host + 'page/index';
+export const indexUrl = host + 'to/index';
 export const toindex = () => {
       window.location.href = indexUrl;
   }
-export const topicUrl = host + 'page/topic';
+export const topicUrl = host + 'to/topic';
 export const totopic = () => {
       window.location.href = topicUrl;
   }
-export const mineUrl = host + 'page/mine';
+export const mineUrl = host + 'to/mine';
 export const tomine = () => {
       window.location.href = mineUrl;
   }
-export const communityUrl = host + 'page/community';
+export const communityUrl = host + 'to/community';
 export const tocommunity = () => {
       window.location.href = communityUrl;
   }
-export const topicsUrl = host + 'page/topics';
+export const topicsUrl = host + 'to/topics';
 export const totopics = () => {
       window.location.href = topicsUrl;
   }
-export const forumUrl = host + 'page/forum';
+export const forumUrl = host + 'to/forum';
 export const toforum = () => {
       window.location.href = forumUrl;
   }
-export const forum_detailUrl = host + 'page/forum_detail';
+export const forum_detailUrl = host + 'to/forum_detail';
 export const toforum_detail = () => {
       window.location.href = forum_detailUrl;
   }
-export const searchUrl = host + 'page/search';
+export const searchUrl = host + 'to/search';
 export const tosearch = () => {
       window.location.href = searchUrl;
   }
-export const search_resultUrl = host + 'page/search_result';
+export const search_resultUrl = host + 'to/search_result';
 export const tosearch_result = () => {
       window.location.href = search_resultUrl;
   }
-export const issueQUrl = host + 'page/issueQ';
+export const issueQUrl = host + 'to/issueQ';
 export const toissueQ = () => {
       window.location.href = issueQUrl;
   }
-export const mineQesUrl = host + 'page/mineQes';
+export const mineQesUrl = host + 'to/mineQes';
 export const tomineQes = () => {
       window.location.href = mineQesUrl;
   }
-export const ourUrl = host + 'page/our';
+export const ourUrl = host + 'to/our';
 export const toour = () => {
       window.location.href = ourUrl;
   }
-export const clockInUrl = host + 'page/clockIn';
+export const clockInUrl = host + 'to/clockIn';
 export const toclockIn = () => {
       window.location.href = clockInUrl;
   }
-export const pictureToQUrl = host + 'page/pictureToQ';
+export const pictureToQUrl = host + 'to/pictureToQ';
 export const topictureToQ = () => {
       window.location.href = pictureToQUrl;
   }
-export const gainUrl = host + 'page/gain';
+export const gainUrl = host + 'to/gain';
 export const togain = () => {
       window.location.href = gainUrl;
   }
