3351a5627dcfaa46c03e348dfb6349aa090e0b1c
joeyang1512 <joeyang1512@gmail.com>
2020-11-04 15:16:58 +0800

    合并dev01

--- src/page/topic/index.js
+++ src/page/topic/index.js
@@ -1,70 +1,235 @@
 import { getTopicAllPart, getTopicByLesson } from '~/ajax/topic';
 import './index.less';
 import { singleTopic } from '../../template/singleChoice';
-import { loading, toastTip } from '../../util/sinceui';
+import { loading, toastTip, EventUtil } from '../../util/sinceui';
 import { multipleTopic } from '../../template/multipleChoice';
-import { getQueryVariable } from '../../../public/js/filters'
-
-// 1. 变量定义
-// localStorage
-let topicHistory = localStorage.getItem('topicHistory');
-let topicStar = localStorage.getItem('topicStar');
-// 获取科目
-let lesson = decodeURI(getQueryVariable('lesson'));
-// 获取科目对应的题目。
-let topicAll = null;
-// 将科目按章节分类
-let topicByPart = {};
-// 2. 获取科目，加载数据
-// 获取科目
-function getLesson(lesson) {
-    switch (lesson) {
-        case 'cpu':
-            console.log('zuyuan')
-            lessonTitle.innerHTML = `${part}`;
-            loadTopic('计算机组成原理');
-            break;
-        case 'net':
-            console.log('jiwang')
-            lessonTitle.innerHTML = `${part}`;
-            loadTopic('计算机网络');
-            break;
-        case 'china':
-            lessonTitle.innerHTML = `${part}`;
-            loadTopic('考研政治');
-            break;
-        case 'link':
-            lessonTitle.innerHTML = `${part}`;
-            loadTopic('数据结构');
-            break;
-        case 'os':
-            lessonTitle.innerHTML = `${part}`;
-            loadTopic('操作系统');
-            break;
-        case 'all':
-            lessonTitle.innerHTML = `${part}`;
-            loadTopic('408综合');
-            break;
-        default:
-            break;
-    };
+import { getQueryVariable } from '../../../public/js/filters';
+let body = document.querySelector('.body');
+let topics = null;
+let topicsOfPart = [];// 存储各章节的题目
+let parts = [];// 各章节目录
+let lesson = decodeURI(getQueryVariable('lesson'));// 科目
+let lessonSelect = document.querySelector('.lessonSelect'); // 章节选择
+let lessonTitle = document.querySelector('.lessonTitle');// 题目
+// ================存取localStorage========================
+let lastIndex = 0;
+let part = localStorage.getItem('part') ? localStorage.getItem('part') : '';
+let starTopic = localStorage.getItem('starTopic') ? localStorage.getItem('starTopic') : '';
+let historyTopic = {
+    cpu:{},
+    net:{},
+    china:{},
+    link:{},
+    os:{},
+    all:{}
 }
-function loadTopic(str) {
+// ========================================================
+// ================获取所有章节=============================
+let obj = { cpu: '计算机组成原理', net: '计算机网络', china: '考研政治', link: '数据结构', os: '操作系统', all: '408综合', };
+getTopicAllPart(obj[lesson]).then(res => {
+    console.log(res);
+
+    for (let i = 0; i < res.data.length; i++) {
+        parts.push(res.data[i].part);
+    }
+    topicsOfPart = new Array(parts.length);
+    if (!part) {
+        lessonTitle.innerHTML = `${parts[0]}`;
+    }
+    lessonSelect.innerHTML = '<div>' + parts.join('</div><div>') + '</div>';
+    lessonSelect.style.display = 'none';
+})
+// ========================================================
+
+// ================获取是哪个科目和章节===========================
+let lessonPart = document.getElementById('lesson');// 右上角章节图标
+lessonPart.onclick = function () {
+    lessonSelect.style.display = lessonSelect.style.display === 'none' ? 'block' : 'none';
+}
+
+lessonSelect.onclick = function (e) {// 确定是哪一个章节
+    lessonSelect.style.display = 'block';
+    let target = e.target;
+    if (target.className === 'lessonSelect') return;
+    part = target.innerText;
+    // localStorage.setItem('part', part);
+    // console.log();
+    let i = parts.indexOf(part);
+    // loadBylesson(null, part);
+    if (part === '全部') {
+        showTopic(body, topics, lastIndex);
+    } else {
+        showTopic(body, topicsOfPart[i], lastIndex);
+    }
+}
+let sectionTopic = null;
+console.log(lesson);
+switch (lesson) {
+    case 'cpu':
+        console.log('zuyuan')
+        lessonTitle.innerHTML = `${part}`;
+        loadBylesson('计算机组成原理');
+        break;
+    case 'net':
+        console.log('jiwang')
+        lessonTitle.innerHTML = `${part}`;
+        loadBylesson('计算机网络');
+
+        break;
+    case 'china':
+        lessonTitle.innerHTML = `${part}`;
+        loadBylesson('考研政治');
+        break;
+    case 'link':
+        lessonTitle.innerHTML = `${part}`;
+        loadBylesson('数据结构');
+        break;
+    case 'os':
+        lessonTitle.innerHTML = `${part}`;
+        loadBylesson('操作系统');
+        break;
+    case 'all':
+        lessonTitle.innerHTML = `${part}`;
+        loadBylesson('408综合');
+        break;
+    default:
+        break;
+}
+// =======================================================
+
+// ================请求数据，显示题目========================
+function loadBylesson(lesson) {
     let load = loading('加载中');
     load(true);
-    getTopicByLesson(str).then(res => {
-        topicAll = res.data;
-        topicToPart(topicAll);
-    })
-};
-// 将科目按章节分类
-function topicToPart(topics) {
-    getTopicAllPart(lesson).then(res => {
-        for (let i = 0; i < res.length; i++) {
-            topicByPart[res[i].part] = topicAll.filter(item => {
-                return item.part === res[i].part;
-            })
+
+    getTopicByLesson(lesson).then(res => {
+        console.log(res);
+        if (res.code === '0') {
+            load(false);
+            topics = res.data;
+            console.log(res)
+            // sectionTopic = topics.map((item) => {
+            //     return item.part = part;
+            // });
+            topicsDivideToParts(topics);
+            showTopic(body, topics, lastIndex);
+        } else {
+            load(false);
+            let toast = toastTip(res.errMsg);
+            toast(true);
+            setTimeout(() => {
+                toast(false);
+            }, 1000);
         }
-    })
+
+    });
 }
-getLesson(lesson);
+
+function topicsDivideToParts(data) {
+    for (let i = 0; i < topicsOfPart.length; i++) {
+        topicsOfPart[i] = data.filter(item => {
+            return item.part === parts[i] ? item : false;
+        });
+    }
+    console.log(topicsOfPart);
+}
+// 展示题目
+function showTopic(element, data, index) {
+    // =======留白--用来判断是否已收藏============
+    // =========================================
+    if (data.length === 0) return;
+    let right = document.getElementById('right');
+    if (data[index].type === 1) {
+        right.style.display = 'none';
+        singleTopic(element, data, index, false);
+        select(1, element, data, index);
+    } else {
+        // let ABCDAll = [];
+        right.style.display = 'block';
+        multipleTopic(element, data, index, false);
+        select(2, element, data, index);
+        right.onclick = () => {
+            multipleTopic(element, data, index, true);
+        }
+    }
+}
+// =========================================================
+
+// ==================上一题和下一题==========================
+let last = document.getElementById('last'),
+    next = document.getElementById('next');
+last.onclick = toLeftTopic; 
+function toLeftTopic() {
+    lastIndex--;
+    if (lastIndex < 0) {
+        let toast = toastTip('亲，这是第一题了哦！');
+        toast(true);
+        setTimeout(() => {
+            toast(false);
+        }, 1000);
+        lastIndex++;
+        return;
+    }
+    showTopic(body, topics, lastIndex);
+    // 讲修改后的lastIndex存进localStorage
+}
+next.onclick = toRightTopic;
+function toRightTopic() {
+    lastIndex++;
+    if (lastIndex >= topics.length) {
+        let toast = toastTip('亲，这是最后一题了哦！');
+        toast(true);
+        setTimeout(() => {
+            toast(false);
+        }, 1000);
+        lastIndex--;
+        return;
+    }
+    showTopic(body, topics, lastIndex);
+    // 讲修改后的lastIndex存进localStorage
+}
+// =========================================================
+
+// ==================返回上一页==========================
+let backBtn = document.getElementById('backBtn');
+backBtn.onclick = function () {
+    window.history.back(-1);
+}
+// =========================================================
+
+// ==================点击选择==========================
+function select(type, element, data, index) {
+    let xuanxiang = document.querySelector('.xuanxiang');
+    xuanxiang.onclick = function (e) {
+        let target = e.target;
+        if (target.tagName === 'IMG') return;
+        while (target !== xuanxiang && target.tagName !== 'LABEL') {
+            target = target.parentNode
+        }
+        console.log(xuanxiang.children[2] === target);
+        if (type === 1) {
+            let ABCD = 1;
+            for (let i = 0; i < xuanxiang.children.length; i++) {
+                if (target === xuanxiang.children[i]) {
+                    console.log('相等')
+                    break;
+                }
+                ABCD++;
+            }
+            console.log(ABCD)
+            if (data[index].answer != ABCD) {
+                target.className = 'weui-cell weui-check__label false';
+            } else {
+                singleTopic(element, data, index, true);
+            }
+        } else {
+            target.className = 'weui-cell weui-check__label select';
+        }
+
+    }
+}
+
+// ====================================================
+
+// 滑动事件监听
+EventUtil.listenTouchDirection(body, true, false, toLeftTopic, false, toRightTopic);
